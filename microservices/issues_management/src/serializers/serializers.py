b6a0ea4a8ab1b1d226e4e397dc9ed30fb60ef1aa
Felipe Cerquera <a.cerquera@uniandes.edu.co>
2024-10-18 08:48:48 -0500

    refactor(issues): Updated issue serializer and provided an api_gateway

--- microservices/issues_management/src/serializers/serializers.py
+++ microservices/issues_management/src/serializers/serializers.py
@@ -5,12 +5,12 @@ from src.serializers.base import BaseSerializer
 class IssueCreateSerializer(BaseSerializer):
     type = fields.String(required=True)
     description = fields.String(required=True)
-    status = fields.String(required=True)
+    status = fields.String()
     source = fields.String(required=True)
     created_at = fields.DateTime()
     updated_at = fields.DateTime(allow_none=True, required=False)
-    user_id = fields.Integer(required=True)
-    company_id = fields.Integer(required=True)
+    user_id = fields.Integer()
+    company_id = fields.Integer()
 
 
 class IssueListSerializer(IssueCreateSerializer):
